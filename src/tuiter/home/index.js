bbc99156741ced58550df593aab42cac5f95af09
ClaraLee1 <97991874+ClaraLee1@users.noreply.github.com>
2022-10-29 23:56:52 -0700

    a7 tuiter

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import PostList from "../post-list";
+import TuitsList from "../tuits/index";
+import WhatsHappening from "./whats-happening";
 
 const HomeComponent = () => {
-    return (
-        <div>
-            <div className="">
-                <PostList/>
-            </div>
-        </div>
-        );
-}
+ return(
+   <>
+     <h4>Home</h4>
+     <WhatsHappening/>
+     <TuitsList/>
+   </>
+ );
+};
 export default HomeComponent;
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,29 +1,3 @@
-// import React from "react";
-// import ExploreComponent from "./explore";
-// import NavigationSidebar from "./navigation-sidebar";
-// import WhoToFollowList from "./who-to-follow-list";
-// import {Routes, Route} from "react-router";
-// import HomeComponent from "./home";
-
-// function Tuiter() {
-//  return (
-//    <div className="row mt-2">
-//      <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-//        <NavigationSidebar active="explore"/>
-//      </div>
-//      <div className="col-10 col-md-10 col-lg-7 col-xl-6"
-//           style={{"position": "relative"}}>
-//        <ExploreComponent/>
-//      </div>
-//      <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-//        <WhoToFollowList/>
-//      </div>
-//    </div>
-//  );
-// }
-
-// export default Tuiter
-
 import React from "react";
 import ExploreComponent from "./explore/index.js";
 import NavigationSidebar
@@ -32,29 +6,39 @@ import WhoToFollowList from "./who-to-follow-list/index.js";
 import {Routes, Route} from "react-router";
 import HomeComponent from "./home";
 import PostSummaryList from "./post-summary-list";
+import whoReducer
+  from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
+import { configureStore }
+  from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+  {reducer: {who: whoReducer, tuits: tuitsReducer}});
 
 function Tuiter() {
     return (
-        <div className='row mt-2'>
-            <div className='col-2 col-md-2 col-lg-1 col-xl-2'>
-                <NavigationSidebar active='explore'/>
+        <Provider store = {store}>
+            <div className='row mt-2'>
+                <div className='col-2 col-md-2 col-lg-1 col-xl-2'>
+                    <NavigationSidebar active='explore'/>
+                </div>
+                <div className='col-10 col-md-10 col-lg-7 col-xl-6'>
+                    {/*style={{"position": "relative"}}*/}
+                    <Routes>
+                        <Route path="" element={<ExploreComponent/>}/>
+                        <Route path="home" element={<HomeComponent/>}/>
+                        <Route path="explore" element={<ExploreComponent/>}/>
+                    </Routes>
+                </div>
+                <div className='col-lg-4 col-xl-4 d-none d-lg-block'>
+                    <Routes>
+                        <Route path="" element={<WhoToFollowList/>}/>
+                        <Route path="home" element={<PostSummaryList/>}/>
+                        <Route path="explore" element={<WhoToFollowList />}/>
+                    </Routes>
+                </div>
             </div>
-            <div className='col-10 col-md-10 col-lg-7 col-xl-6'>
-                {/*style={{"position": "relative"}}*/}
-                <Routes>
-                    <Route path="" element={<ExploreComponent/>}/>
-                    <Route path="home" element={<HomeComponent/>}/>
-                    <Route path="explore" element={<ExploreComponent/>}/>
-                </Routes>
-            </div>
-            <div className='col-lg-4 col-xl-4 d-none d-lg-block'>
-                <Routes>
-                    <Route path="" element={<WhoToFollowList/>}/>
-                    <Route path="home" element={<PostSummaryList/>}/>
-                    <Route path="explore" element={<WhoToFollowList />}/>
-                </Routes>
-            </div>
-        </div>
+        </Provider>
     );
 }
 
